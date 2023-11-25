3d37b9d66d9b81efd41132c6eec7636e442c73b9
OwnSample <ownsample@tutanota.com>
2023-11-25 22:09:22 +0100

    Formeted timing output

--- drawer.py
+++ drawer.py
@@ -26,14 +26,29 @@ def main(stdscr: _curses.window, timeit_ns: Dict[str, int]):
                 i = 2
                 timings_w.box()
                 timings_w.addstr(1, int((y / 2) - (stats) / 2-2), "Timings")
+                longest_key = 0
+                longest_val = 0
                 for k, v in TIME_IT_STAT_NS.items():
-                    timings_w.addstr(i, 1, f"Stat {k} took {v} ns")
+                    if longest_key < len(k):
+                        longest_key = len(k)
+                        timings_w.addstr(i, 1, f"Stat '{k}' ")
+                    else:
+                        timings_w.addstr(i, 1, f"Stat '{k}'")
+                        timings_w.addstr(" " * (longest_key - len(k)+1))
+
+                    if longest_val < len(str(v)):
+                        longest_val = len(str(v))
+                        timings_w.addstr(f"took '{v}' ns")
+                    else:
+                        timings_w.addstr("took ")
+                        timings_w.addstr(" " * (longest_val - len(str(v))))
+                        timings_w.addstr(f"'{v}' ns")
+
                     i += 1
                 timings_w.addstr(i+1, int((y / 2) - (stats) / 2-5), "Press q to close")
                 timings_w.refresh()
-                while True:
-                    if timings_w.getkey() == 'q':
-                        break
+                while timings_w.getkey() != 'q': 
+                    pass
                 timings_p.hide()
             case "q":
                 break
